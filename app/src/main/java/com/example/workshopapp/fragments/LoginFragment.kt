ba70c39529fbbbc7bdc192d198dc24b1977e176c
AgPriyanshu18 <agpriyanshu18@gmail.com>
2023-12-14 14:27:06 +0530

    UI changes Done

--- app/src/main/java/com/example/workshopapp/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/workshopapp/fragments/LoginFragment.kt
@@ -3,11 +3,11 @@ package com.example.workshopapp.fragments
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.workshopapp.Database.DatabaseHelper
 import com.example.workshopapp.R
--- app/src/main/java/com/example/workshopapp/fragments/WorkshopsListFragment.kt
+++ app/src/main/java/com/example/workshopapp/fragments/WorkshopsListFragment.kt
@@ -6,10 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.workshopapp.Adapter.workshopListAdapter
 import com.example.workshopapp.Database.DatabaseHelper
 import com.example.workshopapp.R
